80731de97bd156add932a1ea8d532bfe658fda01
iamsumitkumar10 <sumitku3500@gmail.com>
2024-09-16 23:51:02 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/Header.jsx";
-import About from "./components/About/About.jsx";
+import About from "./components/about/About.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Home from "./components/Home/Home.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Hireme from "./components/Hireme/Hireme.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Hireme from "./components/hireme/Hireme.jsx";
 
  
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <div className="jumbotron">
-        <h1 className="display-4">Hello, world! home page se</h1>
+        <h1 className="display-4">Hello, world! </h1>
         <p className="lead">
           This is a simple hero unit, a simple jumbotron-style component for
           calling extra attention to featured content or information.
